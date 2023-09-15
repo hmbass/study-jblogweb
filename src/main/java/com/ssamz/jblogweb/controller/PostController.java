616b45873d3b28957f30bd2e6b1ea71550b65fd4
hyemin <hmjava.park@gmail.com>
2023-09-15 18:58:50 +0900

    5.2 포스트 목록

--- src/main/java/com/ssamz/jblogweb/controller/PostController.java
+++ src/main/java/com/ssamz/jblogweb/controller/PostController.java
@@ -6,8 +6,12 @@ import com.ssamz.jblogweb.dto.ResponseDTO;
 import com.ssamz.jblogweb.service.PostService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +23,25 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    /*
     @GetMapping({"", "/"})
     public String getPostList(){
+        return "index";
+    }
+     */
+
+    /*
+    @GetMapping({"", "/"})
+    public String getPostList(Model model){
+        model.addAttribute("postList", postService.getPostList());
+        return "index";
+    }
+     */
+
+    @GetMapping({"", "/"})
+    public String getPostList(Model model, @PageableDefault(size = 3, sort = "id",
+    direction = Sort.Direction.DESC) Pageable pageable){
+        model.addAttribute("postList", postService.getPostList(pageable));
         return "index";
     }
 
--- src/main/java/com/ssamz/jblogweb/service/PostService.java
+++ src/main/java/com/ssamz/jblogweb/service/PostService.java
@@ -3,8 +3,12 @@ package com.ssamz.jblogweb.service;
 import com.ssamz.jblogweb.domain.Post;
 import com.ssamz.jblogweb.persistence.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class PostService {
@@ -15,5 +19,16 @@ public class PostService {
     public void insertPost(Post post){
         post.setCnt(0);
         postRepository.save(post);
+    }
+
+    /*
+    @Transactional(readOnly = true)
+    public List<Post> getPostList(){
+        return postRepository.findAll();
+    }
+     */
+    @Transactional(readOnly = true)
+    public Page<Post> getPostList(Pageable pageable){
+        return postRepository.findAll(pageable);
     }
 }
